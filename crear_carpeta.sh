9aa30a69cc8e4f8896d31ddf21d2ac7b0159fb2f
victor <optimuxserver44@gmail.com>
2022-08-22 18:34:25 -0300

    modificando mkdir

--- crear_carpeta.sh
+++ crear_carpeta.sh
@@ -4,34 +4,38 @@
 #FOR para crear
 
 
-set -o errexit  # the script ends if a command fails
-set -o pipefail # the script ends if a command fails in a pipe
-set -o nounset  # the script ends if it uses an undeclared variable
-set -o xtrace # if you want to debug
+#set -o errexit  # the script ends if a command fails
+#set -o pipefail # the script ends if a command fails in a pipe
+#set -o nounset  # the script ends if it uses an undeclared variable
+#set -o xtrace # if you want to debug
 
 
 
 
 #####VARIABLES####
 
-FOLDER_NAME=''
-NEW_FOLDER='ARGENTO'
+#FOLDER_NAME=''
+echo "ESCRIBA EL NOMBRE DEL DIRECTORIO"
+
+read NEW_FOLDER
 
 ####FUNCIONES####
 
 CREAR_CARPETA(){
-        for dir in $@
-                if [ -d $NEW_FOLDER ];then
-                        echo " la carpeta $NEW_FOLDER existe "
+#        for dir in $@
+                if [ -d $NEW_FOLDER ];
+		then
+                        echo "la carpeta $NEW_FOLDER existe"
                 else
                         mkdir $NEW_FOLDER
-                        if [ $? -eq 0 ];then
-                                echo " $NEW_FOLDER se ha creado con exito"
-                        else
-                                echo "Error en creacion de carpeta"
-                        fi
+#                        if [ $? -eq 0 ];
+#			then
+#                               echo " $NEW_FOLDER se ha creado con exito"
+#                        else
+#                                echo "Error en creacion de carpeta"
+#                        fi
                 fi
-        done
+#        done
 
 
 
